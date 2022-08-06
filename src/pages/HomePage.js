841028ba2a60a32373a266829d681751775fac3e
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-08-06 19:50:25 +0430

    Create homepage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Video from "./../components/video/Video";
+
+const HomePage = () => {
+  return (
+    <section className="home-page">
+      <Video />
+    </section>
+  );
+};
+
+export default HomePage;
