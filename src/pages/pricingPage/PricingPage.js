1f45af53f587916868c1575b6d19baf2fa129550
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-08-10 19:07:42 +0430

    Fix something and Create pricing page

---
+++ src/pages/pricingPage/PricingPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import HeroImage from "../../components/heroimage/HeroImage";
+import PricingCards from "../../components/pricing/Pricing";
+
+const PricingPage = () => {
+  return (
+    <section>
+      <Navbar />
+      <HeroImage heading={"PRICING."} text={"Choose your trip."} />
+      <PricingCards />
+    </section>
+  );
+};
+
+export default PricingPage;
