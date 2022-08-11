f009efeaf1f8c02b25dc38a51b184b6dd629b8da
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-08-11 12:10:49 +0430

    Import pages in app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import PricingPage from "./pages/PricingPage";
+import TrainingPage from "./pages/TrainingPage";
+import ContactPage from "./pages/ContactPage";
 
 const App = () => {
   return (
     <>
-      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
+        <Route path="/pricing" element={<PricingPage />}></Route>
+        <Route path="/training" element={<TrainingPage />}></Route>
+        <Route path="/contact" element={<ContactPage />}></Route>
       </Routes>
       <Footer />
     </>
